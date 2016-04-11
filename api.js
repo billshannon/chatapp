a1fe346ecf3d188ffe8f6f5a445e4dcf8a8c3aeb
Bill Shannon <bill@billshannon.com>
2016-04-11 12:01:08 -0600

    add api module

---
+++ api.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var _ = require('lodash');
+var rooms = require("./data/rooms.json");
+var messages = require("./data/messages.json");
+
+
+
+
+var router = express.Router();
+module.exports = router;
+
+
+router.get('/rooms', function(req,res) {
+    res.json(rooms);
+
+});
+
+router.get('/rooms/:roomId/messages', function(req,res) {
+
+    var roomId = req.params.roomId;
+
+
+    var roomMessages = messages
+    .filter(m => m.roomId === roomId)
+
+    var room = _.find(rooms, r => r.id === roomId);
+    if(!room) {
+        res.sendStatus(404);
+        return
+    }
+
+    res.json({
+        room: room,
+        messages: roomMessages
+    })
+
+});
